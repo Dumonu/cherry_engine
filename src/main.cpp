5e9d6d51272fcd2555754d2663bba335f60bd163
Dumonu <jacobcherry36@gmail.com>
2020-06-02 22:54:26 -0500

    Build system up and running.
    Wow, CMake is nice.

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,12 @@
 #include <stdio.h>
 
+#include "version.h"
+
 int main(int argc, char** argv)
 {
+    printf("CherryEngine version %d.%d.%d.%x\n", VERSION_MAJOR, VERSION_MINOR, 
+        VERSION_REVISION, VERSION_TWEAK);
+
     for(int i = 0; i < argc; ++i)
     {
         printf("%s%s", argv[i], i == argc - 1 ? "\n" : " ");
